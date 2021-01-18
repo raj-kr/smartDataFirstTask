45e020c037482336cf80f73b91b2931d29ec4d80
raj-kr <rkgt76@gmail.com>
2021-01-18 17:19:02 +0530

    keys updation

--- server/config/index.js
+++ server/config/index.js
@@ -6,8 +6,8 @@ if (process.env.ENVIRONMENT === 'development') {
     keys.client = process.env.LOCAL_CLIENT_PATH;
     keys.server = process.env.LOCAL_SERVER_PATH;
 } else {
-    keys.client = process.env.LOCAL_CLIENT_PATH;
-    keys.server = process.env.LOCAL_SERVER_PATH;
+    keys.client = process.env.LIVE_CLIENT_PATH;
+    keys.server = process.env.LIVE_SERVER_PATH;
 }
 keys.port = process.env.PORT || 3001;
 keys.dbAddress = process.env.MONGO_URL;
